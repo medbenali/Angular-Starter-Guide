a292e548120ca06f70b3991549fa71e447e5efce
Mohamed BEN ALI <mohamed.benali@esprit.tn>
2023-08-03 10:49:07 +0100

    Added Sales Project Code With Integrating Bootstrap

---
+++ 5-angular-integrate-angular-and-bootstrap-table/sales-project/src/app/sales-person-list/sales-person-list.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { SalesPerson } from './sales-person';
+
+@Component({
+  selector: 'app-sales-person-list',
+  templateUrl: './sales-person-list-bootstrap.component.html',
+  styleUrls: ['./sales-person-list.component.css']
+})
+export class SalesPersonListComponent {
+
+  // create an array of objects
+
+  salesPersonList: SalesPerson[] = [
+
+    new SalesPerson("Mohamed", "BEN ALI", "mohamed.benali@esprit.tn", 50000),
+    new SalesPerson("John", "Doe", "john.doe@gmail.com", 40000),
+    new SalesPerson("Claire", "Murphy", "claire.murphy@gmail.com", 90000),
+    new SalesPerson("Mai", "Truong", "mai.truong@gmail.com", 60000)
+
+  ];
+
+}
